a6f418a0c870caca667b62f786e595a5439048ff
JrPD <tsurkan_i@mail.ru>
2014-11-23 23:55:18 +0200

    Intership beginning

---
+++ Intership/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intership
+{
+
+    public static class Func
+    {
+        public static T Clone<T>(this T toClone)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, toClone);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+   
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int i = 5;
+            int b;
+            b = i.Clone();
+
+            4.Clone();
+            var str = "asdfasdf".Clone();
+            var v = new object().Clone();
+
+            Console.WriteLine(i);
+            Console.WriteLine(b);
+
+            Console.WriteLine(4.Clone());
+
+            Console.WriteLine(str);
+            Console.WriteLine(v);
+
+            Console.ReadKey();
+
+        }
+    }
+}
