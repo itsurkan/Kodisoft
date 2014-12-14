964edcdee5a50074f4580f11ec7476be60cbedba
Bohdan127 <benetskyybogdan@gmail.com>
2014-12-15 00:49:47 +0100

    Update Program.cs
    add last functions

--- References6/Program.cs
+++ References6/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 
@@ -9,40 +11,39 @@ namespace References6
 
     class Program
     {
-
+        public static Dictionary<Type,object> dictionary = new Dictionary<Type, object>();
+        public static List<object> NewItem = new List<object>();
 
         static void Main(string[] args)
         {
-            var dictionary = new Dictionary<Type, object>();
-
             try
             {
                 foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
-                {
-                    var temp = assem.GetTypes()[0].GetConstructor(Type.EmptyTypes);
-                    if (temp != null)
-                        dictionary.Add(assem.GetTypes()[0],temp);
-                }
+                    if (assem.GetTypes()[0].GetConstructor(Type.EmptyTypes) != null)
+                        dictionary.Add(assem.GetTypes()[0], assem.GetTypes()[0].GetConstructor(Type.EmptyTypes));
+                
                 foreach (var item in dictionary)
                 {
-                    var sb = new StringBuilder();
-                    sb.AppendFormat("Key = {0}", item.Key);
-                    sb.AppendLine();
-                    sb.AppendFormat("Value = {0}", item.Value);
-                    sb.AppendLine();
-                    Console.WriteLine(sb);
+                    NewItem.Add(Create(item.Key));
+                    Console.WriteLine("Key = {0}", item.Key);
+                    Console.WriteLine("Value = {0}", item.Value);
+                    Console.WriteLine(NewItem.Last());
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error");
-                Console.WriteLine(e.Data);
+                Console.WriteLine(e.Message);
             }
-
-            Console.ReadKey();
+          
+            //Console.ReadKey();
         }
 
-
+        public static object Create(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+      
 
     }
 }
