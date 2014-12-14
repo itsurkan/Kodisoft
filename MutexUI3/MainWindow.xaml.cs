17ffa89fdb3d3d62eac369816dea74a6d270c744
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-14 20:01:41 +0200

    5.0

---
+++ MutexUI3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+
+namespace MutexUI3
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private async void LockUIasync_OnClick(object sender, RoutedEventArgs e)
+        { 
+            AsyncLock lockUI = new AsyncLock();
+
+            using (await lockUI.LockSection())
+            { 
+                lTest.Content = "Using block";
+            }
+        }
+
+        private async void LockUIasyncLR_OnClick(object sender, RoutedEventArgs e)
+        {
+            AsyncLock mutexUI = new AsyncLock();
+            await mutexUI.Lock();
+           
+            lTest.Content = "Using Lock-Release"; 
+            mutexUI.Release();
+        }
+    }
+}
+
+
