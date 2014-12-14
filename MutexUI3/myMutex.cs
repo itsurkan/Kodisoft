e267e8daf9d0ce7c482f6cbc2857c4c5a768e33f
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-14 19:59:17 +0200

    1-5

---
+++ MutexUI3/myMutex.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MutexUI3
+{
+    public class AsyncLock
+    {
+        // Using lock-release
+        Mutex mutx = new Mutex();
+        Dispatcher UserDispatcher;
+
+        public async Task Lock()
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                mutx.WaitOne();
+                Console.WriteLine("ThreadOne, executing ThreadMethod, " +
+                    "is {0} from the thread pool.",
+                    Thread.CurrentThread.ManagedThreadId);
+                UserDispatcher = Dispatcher.CurrentDispatcher;
+            });
+        }
+
+        public void Release()
+        {
+            try
+            {
+                if (mutx != null && UserDispatcher != null)
+                {
+                    //UserDispatcher.Invoke(new Action(() =>
+                    //{
+                    //    Console.WriteLine("Releasing...");
+                       mutx.ReleaseMutex();
+                    //}));
+                }
+                else
+                {
+                    Console.WriteLine("Null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        // Using block
+        private readonly Task<IDisposable> _releaserTask;
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 2);
+        private readonly IDisposable _releaser;
+
+        public AsyncLock()
+        {
+            _releaser = new Releaser(_semaphore);
+            _releaserTask = Task.FromResult(_releaser);
+        }
+        public Task<IDisposable> LockSection()
+        {
+            Task waitTask = _semaphore.WaitAsync();
+            return waitTask.IsCompleted
+                ? _releaserTask
+                : waitTask.ContinueWith(
+                    (_, releaser) => (IDisposable)releaser,
+                    _releaser,
+                    CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
+        }
+
+        private class Releaser : IDisposable
+        {
+            private readonly SemaphoreSlim _semaphore;
+
+            public Releaser(SemaphoreSlim semaphore)
+            {
+                _semaphore = semaphore;
+            }
+
+            public void Dispose()
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
---
+++ References6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace References6
+{
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Dictionary<Type, object> dictionary = new Dictionary<Type, object>();
+
+            Console.WindowWidth = 150;
+
+            try
+            {
+                foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    dictionary.Add(assem.GetTypes()[0], assem.GetName().Name);
+                }
+                foreach (var item in dictionary)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendFormat("Key = {0}", item.Key);
+                    sb.AppendLine();
+                    sb.AppendFormat("Value = {0}", item.Value);
+                    sb.AppendLine();
+                    Console.WriteLine(sb);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(e.Data);
+            }
+          
+            Console.ReadKey();
+        }
+
+      
+
+    }
+}
