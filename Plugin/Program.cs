1c4a598ad33468f9486af113f8eaccab3085c562
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-17 21:38:50 +0200

    Random Generator enqueue

--- Plugin/Program.cs
+++ Plugin/Program.cs
@@ -6,23 +6,15 @@ namespace Plugin
     {
         static void Main(string[] args)
         {
+            PluginContainer list = new PluginContainer();
+            list.Modify("");
+            Console.WriteLine();
 
-            WorkWithDouble temp1 = new WorkWithDouble();
-            temp1.DataOutput();
-
-            WorkWithString temp2 = new WorkWithString();
-            temp2.DataOutput();
-
-            WorkWithInt temp3 = new WorkWithInt();
-            // temp3.Data =4;
-            temp3.DataOutput();
-
-            GeneralModifyInt temp4 = new GeneralModifyInt();
-            temp4.Data = 2;
-           // temp4.ModifyData = 4;
-            temp4.DataOutput();
-
-            Console.ReadKey();
+            BasePluginWork<int> basePlugin = new BasePluginWork<int>();
+            basePlugin.Data = 5;
+            basePlugin.DataOutput();
+            
+            //Console.ReadKey();
         }
     }
 }
---
+++ RandomAPI_5/GeneratorInts.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using org.random.JSONRPC;
+
+namespace RandomAPI_5
+{
+    class Generator
+    {
+        private  Queue<int> localQ;
+        private  int localQSize;
+
+        public Generator(int limit)
+        {
+            localQSize = limit;
+            localQ = new Queue<int>(localQSize);
+        }
+
+        public  int Enqueue()
+        {
+            try
+            {
+                if (localQ.Count == 0)
+                {
+                    Task loader = Task.Factory.StartNew(() => APISequenceGenerator(localQSize));
+                    Task.WaitAll(loader);
+                }
+                return localQ.Dequeue();
+            }
+            catch (Exception)
+            {
+                Task.Factory.StartNew(() => BCLSequenceGenerator(localQSize));
+                return localQ.Dequeue();
+            }
+        }
+
+        /// <summary>
+        /// API Sequence Generator. Not always the technical task
+        /// Random.org
+        /// </summary>
+        private void APISequenceGenerator(int maxInt)
+        {
+            const string apiKey = "8900240a-a7f2-433d-ad11-d3ca1987cc05";
+            try
+            {
+                RandomJSONRPC client = new RandomJSONRPC(apiKey);
+                int[] tmp = client.GenerateIntegers(maxInt, 0, maxInt);
+
+                foreach (var num in tmp)
+                    localQ.Enqueue(num);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("API error");
+
+                // Use BCL generator;
+                BCLSequenceGenerator(maxInt);
+            }
+        }
+
+        /// <summary>
+        /// BCL Sequence Generator
+        /// From Random.org
+        /// </summary>
+        private void BCLSequenceGenerator(int maxInt)
+        {
+            Random rng = new Random();
+
+            for (int i = 0; i < maxInt; i++)
+                localQ.Enqueue(rng.Next(0, maxInt));
+        }
+
+        public void PrintQueue()
+        {
+            int i = 0;
+            while (localQ.Count() != 0)
+            {
+                Console.CursorLeft = 4 * (i % 10);
+                Console.Write(localQ.Dequeue() + " ");
+
+                if ((i != 0) && ((i + 1) % 10 == 0))
+                {
+                    Console.CursorLeft = 0;
+                    Console.CursorTop += 1;
+                }
+                i++;
+            }
+        }
+    }
+}
