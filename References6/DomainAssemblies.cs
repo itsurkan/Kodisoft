5db989187079700896246b5179ae4e79d83df8dd
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-17 21:43:51 +0200

    release

---
+++ References6/DomainAssemblies.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace References6
+{
+    public class DomainAssemblies
+    {
+        private object locker;
+
+        public Dictionary<Type, ConstructorInfo> Dictionary;
+
+        public DomainAssemblies()
+        {
+            Dictionary = new Dictionary<Type, ConstructorInfo>();
+            locker = new object();
+        }
+
+        public static object Create(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+
+        public void FindReferences()
+        {
+            lock (locker)
+            {
+                try
+                {
+                    foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        var type = assembly.GetTypes()[0];
+                        if (type.GetConstructor(Type.EmptyTypes) != null)
+                            lock (Dictionary)
+                                if (!Dictionary.ContainsKey(type))
+                                    Dictionary.Add(type, type.GetConstructor(Type.EmptyTypes));
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public void PrintRef()
+        {
+            foreach (var item in Dictionary)
+            {
+                lock (Dictionary)
+                    Console.WriteLine("Key = {0}", item.Key);
+                    Console.WriteLine("Value = {0}", item.Value);
+                    Console.WriteLine();
+            }
+        }
+    }
+}
--- References6/Program.cs
+++ References6/Program.cs
@@ -1,75 +1,17 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-
+using System.Reflection;
 
 namespace References6
 {
-    public class WorkwithDomains
-    {
-        public Dictionary<Type, object> Dictionary;
-
-        public List<object> NewItem;
-
-        private  object locker;
-
-        public WorkwithDomains()
-        {
-            Dictionary = new Dictionary<Type, object>();
-            NewItem = new List<object>();
-            locker = new object();
-        }
-
-        public static object Create(Type type)
-        {
-                return Activator.CreateInstance(type);
-        }
-
-        public void DoOurWorkToTheEnd()
-        {
-            lock (locker)
-            {
-                try
-                {
-                    foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
-                            if (assem.GetTypes()[0].GetConstructor(Type.EmptyTypes) != null)
-                               lock(Dictionary) 
-                                    Dictionary.Add(assem.GetTypes()[0], assem.GetTypes()[0].GetConstructor(Type.EmptyTypes));
-                        
-
-                    foreach (var item in Dictionary)
-                    {
-                        lock (NewItem)
-                            NewItem.Add(Create(item.Key));
-                            Console.WriteLine(NewItem.Last());
-
-                            Console.WriteLine("Key = {0}", item.Key);
-                            Console.WriteLine("Value = {0}", item.Value);
-                            Console.WriteLine();
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error");
-                    Console.WriteLine(e.Message);
-                }
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-           WorkwithDomains domainsWorker = new WorkwithDomains();
-           domainsWorker.DoOurWorkToTheEnd();
+            DomainAssemblies domainsWorker = new DomainAssemblies();
+            domainsWorker.FindReferences();
+            domainsWorker.PrintRef();
             //Console.ReadKey();
         }
-
-       
-
-    
     }
 }
