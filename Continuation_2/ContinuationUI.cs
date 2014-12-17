4c80ce7a9d110a52e77cd0c6e7fb8635920461ea
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-18 00:44:35 +0200

    preRelease

---
+++ Continuation_2/ContinuationUI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public static class Continuation
+{
+
+    public static Task ContinueUI(this Task task, Action action)
+    {
+        return task.ContinueWith(t => action.Invoke(),
+            TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    public static Task ContinueUI(this Task task, Action<Task> action)
+    {
+        return task.ContinueWith((t) => action.Invoke(t),
+            TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    public static Task<T> ContinueUI<T>(this Task<T> task, Func<T> action)
+    {
+        return task.ContinueWith(t => action.Invoke(), 
+            TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+    public static Task<T> ContinueUI<T>(this Task<T> task, Func<Task, T> action)
+    {
+        return task.ContinueWith((t) => action.Invoke(t), 
+            TaskScheduler.FromCurrentSynchronizationContext());
+    }
+
+}
--- Continuation_2/MainWindow.xaml.cs
+++ Continuation_2/MainWindow.xaml.cs
@@ -1,55 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Continuation_2
 {
-  public  static class Continuation
-     {
+ 
 
-         public static Task ContinueTask(this Task task, Action continuationAction)
-         {
-             Task UITask = task.ContinueWith((arg) =>
-             {
-                 MessageBox.Show(DateTime.Now.Second.ToString());
-             }, TaskScheduler.FromCurrentSynchronizationContext());
-             return UITask;
-         }
-
-         public  static Task ContinueTask(this Task task, Action<Task> continuationAction)
-         {
-             Task UITask = task.ContinueWith((arg) =>
-             {
-                 MessageBox.Show(DateTime.Now.Second.ToString());
-             }, TaskScheduler.FromCurrentSynchronizationContext());
-             return UITask;
-         }
-
-         public static Task ContinueTask<T>(Func<T> function, Func<Task<T>> TaskFunction)
-         {
-             return null;
-         }
-         public static Task ContinueTask<T>(Func<Task<T>> TaskFunction, Func<Task, Task<T>> newFunc)
-         {
-             return null;
-         }
-     }
-
-
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
@@ -58,21 +15,21 @@ namespace Continuation_2
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            Task task = new Task(() => { MessageBox.Show("Hello from first"); }  );
-            
-            task.ContinueTask(Test);
-            task.Start();
+            Task task1 = Task.Run(() => Test());
+            task1.ContinueUI(()=>{label1.Content = "Action Finished";});
+            label1.Content = "Action in process";
         }
-
-        public void Test()
-        {
-            label1.Content = "Test continue with";
-        }
-
+        
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("It is OK");
+            Task.Factory.StartNew(t => Test(),  TaskScheduler.Default).
+                ContinueUI(t => label1.Content = "Action<Task> test finished");
+            label1.Content = "Action in process";
         }
-
+        
+        private void Test()
+        {
+            Thread.Sleep(5000);
+        }
     }
 }
