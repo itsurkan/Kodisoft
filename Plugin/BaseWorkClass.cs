c0562e75fd0757248ef5a3377da82a7e9b480817
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-19 13:26:39 +0200

    refactoring v1.5+factory

--- Plugin/BaseWorkClass.cs
+++ Plugin/BaseWorkClass.cs
@@ -1,46 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Plugin.Plugins;
 
 
 namespace Plugin
 {
+    public  class Factory
+    {
+        public static IPlugin CreatePlugin(Type plugType)
+        {
+            switch (plugType.Name)
+            {
+                case "Int32":
+                    return new WithInt();
+
+                case "Double":
+                    return new WithDouble();
+
+                case "String":
+                    return new WithString();
+
+                default:
+                    Console.WriteLine("Unresolved type for PLugins. " + plugType);
+                    break;
+            }
+            return null;
+        }
+    }
 
     public class BasePluginWork<T>
     {
-        public T ModificationData { get; set; }
+        public T Data { get; set; }
         private Type plugType;
         public IPlugin CurrentPlugin { get; set; }
-        private readonly string unsupported = "Sorry but this type is not suported in current version.";
+        protected readonly string unsupported = "Sorry but this type is not suported in current version.";
 
         public BasePluginWork()
         {
-            plugType = typeof(T);
-
-            try
-            {
-                if (typeof (T) == typeof (double))
-                    CurrentPlugin = new WithDouble();
-
-                else if (typeof (T) == typeof (int))
-                    CurrentPlugin = new WithInt();
-
-                else if (typeof (T) == typeof (string))
-                    CurrentPlugin = new WithString();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Errow occured creatinf instance: BasePluginWork");
-            }
+            plugType = typeof (T);
+            CurrentPlugin = Factory.CreatePlugin(plugType);
         }
-
-        public void DataOutput()
+  
+    public void DataOutput()
         {
-            Console.WriteLine("Modified data by base class for plugins");
+            Console.WriteLine("Modified data by base class for plugins.  " + plugType.Name);
             try
             {
                 if ((CurrentPlugin as IModify<T>) != null)
-                    Console.WriteLine((CurrentPlugin as IModify<T>).Modify(ModificationData));
+                    Console.WriteLine((CurrentPlugin as IModify<T>).Modify(Data));
             }
             catch (Exception)
             {
--- Plugin/Program.cs
+++ Plugin/Program.cs
@@ -13,7 +13,17 @@ namespace Plugin
 
             BasePluginWork<int> plugin = new BasePluginWork<int>();
             plugin.Data = 5;
+
+            BasePluginWork<double> plugin1 = new BasePluginWork<double>();
+            plugin1.Data = -65;
+
+            BasePluginWork<string> plugin2 = new BasePluginWork<string>();
+            plugin2.Data = "It is my life";
+           
             plugin.DataOutput();
+            plugin1.DataOutput();
+            plugin2.DataOutput();
+            Console.ReadKey();
         }
     }
 }
