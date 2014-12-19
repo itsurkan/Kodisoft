ab51d1f7e4eb8c75643f048393170cc07d44b938
Ivan Tsurkan <tsurkan_i@mail.ru>
2014-12-19 13:29:38 +0200

    factory

---
+++ Plugin/Factory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Plugin.Plugins;
+
+
+namespace Plugin
+{
+    public class Factory
+    {
+        public static IPlugin CreatePlugin(Type plugType)
+        {
+            switch (plugType.Name)
+            {
+                case "Int32":
+                    return new WithInt();
+
+                case "Double":
+                    return new WithDouble();
+
+                case "String":
+                    return new WithString();
+
+                default:
+                    Console.WriteLine("Unresolved type for PLugins. " + plugType);
+                    break;
+            }
+            return null;
+        }
+    }
+}
+
